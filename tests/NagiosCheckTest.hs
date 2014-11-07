0a25460017b13c35099c04f35380522994127d75
Sharif Olorin <sio@tesser.org>
2014-11-07 21:25:29 +1100

    Fix test

--- tests/NagiosCheckTest.hs
+++ tests/NagiosCheckTest.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import Data.Text (Text)
 import Test.Hspec
 
 import System.Nagios.Plugin
@@ -11,12 +12,18 @@ main = hspec suite
 
 suite :: Spec
 suite = describe "runNagiosPlugin'" $ it
-    "returns the worst result on completion" $ do
-        (_,(rs,pds)) <- runNagiosPlugin' universeCheck
+    "returns explicitly added result in favor of default" $ do
+        (_,(rs,pds)) <- runNagiosPlugin' (universeCheck pi)
         pds `shouldBe` []
-        checkInfo (maximum rs) `shouldBe` "universe broken, π < 3"
+        checkInfo (maximum rs) `shouldBe` universeGoodResult
 
-universeCheck :: NagiosPlugin ()
-universeCheck = do
-    addResult OK "universe passes basic consistency tests"
-    if (pi < (3.0 :: Double)) then addResult Critical "universe broken, π < 3" else return ()
+universeCheck :: Double -> NagiosPlugin ()
+universeCheck pi' = do
+    addResult OK universeGoodResult
+    if (pi' < (3.0 :: Double)) then addResult Critical universeBadResult else return ()
+
+universeGoodResult :: Text
+universeGoodResult = "universe passes basic consistency tests"
+
+universeBadResult :: Text
+universeBadResult = "universe broken, π < 3"
